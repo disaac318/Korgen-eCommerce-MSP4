72a99d3854f6bd587faeee06c588032c7386cba5
Dean Isaac <shuttermaven@yahoo.ca>
2026-02-21 16:44:29 +0000

    initial commit

---
+++ korgen_eCom/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for korgen_eCom project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'korgen_eCom.settings')
+
+application = get_wsgi_application()
