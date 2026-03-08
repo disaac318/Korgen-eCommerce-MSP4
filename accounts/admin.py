33b8ebaac82157033582cc0fcec7c3de0207a899
Dean Isaac <shuttermaven@yahoo.ca>
2026-03-08 17:19:27 +0000

    enable cusyum password field to read-only

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
-admin.site.register(Account)
+
+class AccountAdmin(UserAdmin):
+    list_display = (
+        'email', 'first_name', 'last_name', 'username',
+        'date_joined', 'last_login', 'is_admin',
+        'is_active', 'is_staff', 'is_superuser'
+    )
+
+#     list_display_links = ('email', 'first_name', 'last_name')
+#     ordering = ('-date_joined',)
+
+#     search_fields = ('email', 'username')
+    readonly_fields = ('date_joined', 'last_login')
+
+
+admin.site.register(Account, AccountAdmin)
