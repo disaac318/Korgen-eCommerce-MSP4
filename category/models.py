81c7a38e706a8dffd381e03d341948195c5eeeeb
Dean Isaac <shuttermaven@yahoo.ca>
2026-03-08 02:13:28 +0000

    modified footer.html and models.py for some typo errors

--- category/models.py
+++ category/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-     name = models.CharField(max_length=50, unique=True)
-     slug = models.SlugField(max_length=100, unique=True)
-     description = models.CharField(max_length=255, blank=True)
+     category_name = models.CharField(max_length=50, unique=True)
+     slug = models.CharField(max_length=100, unique=True)
+     description = models.TextField(max_length=255, blank=True)
      cat_image = models.ImageField(upload_to='photos/categories/', blank=True, null=True)
 
      def  __str__(self):
-          return selfcategory.name
+          return self.category_name
