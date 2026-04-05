5776429918900c0141660dd063613dadd37003f9
Dean Isaac <shuttermaven@yahoo.ca>
2026-04-05 20:58:26 +0100

    display products in store page

--- home/views.py
+++ home/views.py
@@ -3,8 +3,8 @@ from store.models import Product
 
 # Create your views here.
 def index(request):
-     products = Product.objects.filter(is_available=True)
-     context = {
+    products = Product.objects.filter(is_available=True)
+    context = {
           'products': products,
      }
-     return render(request, 'home/index.html', context)
+    return render(request, 'home/index.html', context)
--- store/views.py
+++ store/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
 def store(request):
-     return render(request, 'store/store.html')
+    products = Product.objects.filter(is_available=True)
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'store/store.html', context)
