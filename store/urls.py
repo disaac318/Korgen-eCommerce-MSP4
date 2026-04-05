176686c7dd963bb7bff52ac49cbf03186e238f96
Dean Isaac <shuttermaven@yahoo.ca>
2026-04-05 22:57:11 +0100

    display products by category

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
-
+    path('category/<slug:category_slug>/', views.store,
+          name='products_by_category'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from category.models import Category
+
 from .models import Product
 
 
 # Create your views here.
-def store(request):
+def store(request, category_slug=None):
     products = Product.objects.filter(is_available=True)
+
+    if category_slug is not None:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
     product_count = products.count()
     context = {
         'products': products,
