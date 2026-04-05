a81496d8479679ee618c86664a00ee2b2166e794
Dean Isaac <shuttermaven@yahoo.ca>
2026-04-05 21:12:24 +0100

    live count display updated

--- store/views.py
+++ store/views.py
@@ -5,8 +5,10 @@ from .models import Product
 # Create your views here.
 def store(request):
     products = Product.objects.filter(is_available=True)
+    product_count = products.count()
     context = {
         'products': products,
+        'product_count_label': f"{product_count} item{'s' if product_count != 1 else ''} found.",
     }
 
     return render(request, 'store/store.html', context)
