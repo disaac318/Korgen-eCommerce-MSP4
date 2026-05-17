d5f602e4f92900f99853cc1c669da3ecec490c42
Dean Isaac <shuttermaven@yahoo.ca>
2026-05-17 22:45:08 +0100

    show rating stars on home and store page

--- home/views.py
+++ home/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from store.models import Product
 
-# Create your views here.
+from store.models import Product, ReviewRating
+
+
 def index(request):
-    products = Product.objects.filter(is_available=True)
+    products = Product.objects.filter(is_available=True).order_by('created_date')
+
+    # Get the reviews
+    reviews = None
+    for product in products:
+        reviews = ReviewRating.objects.filter(product=product, status=True)
+
     context = {
-          'products': products,
-     }
+        'products': products,
+        'reviews': reviews,
+    }
+
     return render(request, 'home/index.html', context)
