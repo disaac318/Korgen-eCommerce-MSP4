597739f59e5154bf2a635b0b069ec28943051637
Dean Isaac <shuttermaven@yahoo.ca>
2026-05-18 11:20:48 +0100

    update development in settings.p

--- korgen_eCom/settings.py
+++ korgen_eCom/settings.py
@@ -42,7 +42,7 @@ def config_bool(name, default=False):
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config_bool('DEBUG', default=True)
+DEBUG = config_bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
